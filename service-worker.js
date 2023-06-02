8acf981008e903875ad38ba5fb342f11a5fac848
Fabio Romeiro <hi@fabioromeiro.dev>
2023-06-02 18:38:03 -0300

    Create chrome extension to chat with select number on Whatsapp

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.contextMenus.create({
+		id: 'chamarNoZap',
+		title: 'Chamar "%s" no Whatsapp Web',
+		type: 'normal',
+		contexts: ['selection'],
+	});
+
+	chrome.contextMenus.onClicked.addListener(({ selectionText }) => {
+		const formattedNumber = selectionText;
+		const whatsappWebBaseURL = 'https://web.whatsapp.com/send';
+		chrome.tabs.create({
+			url: `${whatsappWebBaseURL}/?phone=${formattedNumber}type=phone_number`
+		});
+	})
+});
